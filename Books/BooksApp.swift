fe72123a8c445412db1648a1597a01a069a2d1d7
Uday <udaysidhu1@outlook.com>
2023-01-24 21:12:41 +0530

    Initial Commit

---
+++ Books/BooksApp.swift
@@ -0,0 +1,17 @@
+//
+//  BooksApp.swift
+//  Books
+//
+//  Created by Uday Sidhu on 24/01/23.
+//
+
+import SwiftUI
+
+@main
+struct BooksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
